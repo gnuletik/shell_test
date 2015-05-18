e84c0a80be0d9d2b1058167d23e82ae9489b59a4
desrum_m <martin.desrumaux@epitech.eu>
2015-05-18 15:48:53 +0200

    First commit

---
+++ test.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# TO DO
+# Rediriger vers un fichier la difference et le supprimer seulement si le test a succed ?
+
+RCol='\e[0m'
+BBla='\e[1;33m';
+BRed='\e[1;31m';
+
+SHELL=./42sh
+TCSH=tcsh
+
+trap 'if [[ $? -eq 139 ]]; then echo "segfault !"; fi' CHLD
+
+LS_BEGIN=`ls`
+
+if [ ! -x ./42sh ]; then
+  echo "Can't open your shell $SHELL"
+  exit
+fi
+
+############### Mettez des tests !! ############### 
+
+declare -a tests=(
+
+"cat Makefile" 
+"Unexisting"
+"ls | grep include"
+"ls | grep e | grep a > toto ; cat toto ; rm toto"
+"ls > cul | ls < EOF"
+"cat toto"
+
+# etc...
+)
+
+for i in "${tests[@]}"
+do
+
+echo "Test : $i"
+
+#Compare stdout
+echo -en "\tSTDOUT : "
+diff <( echo "$i" | $SHELL 2>/dev/null )     <( echo "$i" | $TCSH 2>/dev/null )	   > /dev/null \
+    && echo -e "${BBla}OK${RCol}" || echo -e "${BRed}KO${RCol}"
+
+#Compare stderr
+echo -en "\tSTDERR : "
+diff <( echo "$i" | $SHELL 2>&1 1>/dev/null) <( echo "$i" | $TCSH 2>&1 1>/dev/null ) > /dev/null \
+    && echo -e "${BBla}OK${RCol}" || echo -e "${BRed}KO${RCol}"
+
+#Compare return value
+(echo "$i" | $SHELL 2>&1) >/dev/null
+res1=$?
+(echo "$i" | $TCSH 2>&1) >/dev/null
+res2=$?
+
+echo -en "\tRETURN : "
+[ $res1 == $res2 ] && echo -e "${BBla}OK${RCol}" || echo -e "${BRed}KO${RCol}"
+
+done
+
+LS_END=`ls`
+
+#Know if files were created
+if [[ $LS_BEGIN != $LS_END ]] ; then
+  echo -e "${BRed}Some files were created${RCol}"
+fi
