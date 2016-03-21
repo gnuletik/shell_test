6f811aace36250956372f18a816fb432babeea87
vanhec_a <alexandre.vanhecke@epitech.eu>
2016-03-21 15:23:55 +0100

    Add minishell2 mode

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # shell_test
-A shell script to test a sell. To see if it have the same out of tcsh
+
+Shell script to test mysh / 42sh
+
+## Usage :
+
+- Copy/Paste your mysh/42sh binaries in the same folder than the test.sh
+- ./test.sh
--- test.sh
+++ test.sh
@@ -1,70 +1,101 @@
 #!/bin/bash
 
-# TO DO
-# Rediriger vers un fichier la difference et le supprimer seulement si le test a succed ?
-# Only one execution !!
-# Option pour tester avec valgrind
-# Option pour donner un fichier de test
-# tput instead of static termcaps ?
-
 RCol='\e[0m'
-BBla='\e[1;33m';
+BBla='\e[1;32m';
 BRed='\e[1;31m';
 
 SHELL=./42sh
 TCSH=tcsh
 
-trap 'echo -en "${BRed}SEGFAULT${RCol}" && rm core.*' SIGSEGV
+trap 'echo -en "${BRed}SEGFAULT ${RCol}" && rm core.*' SIGSEGV
 
 LS_BEGIN=`ls`
 
 if [ ! -x ./42sh ]; then
-  echo "Can't open your shell $SHELL"
-  exit
+    if [ ! -x ./mysh ]; then
+	echo "Can't open your mysh/42sh"
+	exit
+    else
+	echo "Exécution en mode minishell2"
+	SHELL=./mysh
+    fi
+else
+    echo "Exécution en mode 42sh"
 fi
 
-############### Mettez des tests !! ############### 
+############### Mettez des tests !! ###############
 
-declare -a tests=(
+if [ $SHELL = "./42sh" ]; then
 
-"cat Makefile" 
-"Unexisting"
-"pwd && cd .. && cd - && pwd"
-"ls | grep include"
-"ls|grep include"
-"ls | grep include && echo ok || echo ko"
-"ls | grep e | grep a > toto ; cat toto ; rm toto"
-"ls > cul | ls < EOF"
-"ls | > cul"
-"cat toto"
-"cat -e Makefile | grep 'CC'"
-"ls /root || ls"
-"cat << EOF | less"
-"ls;ls"
-"ls ; ls"
-"|"
-""
-"&"
-"&&"
-"||"
-";"
-";;"
-"who"
-"cat << EOF\ncoucou\nsalut\nEOF"
-"echo echo echo ls | $SHELL | $SHELL | $SHELL "
-"touch toto ; touch test ; cat < toto | wc -l < test ; rm toto ; rm test"
-"mkdir lol && touch lol/test && ls > ls_out lol ; cat ls_out ; rm -Rf lol ; rm -f ls_out"
-"echo /*/*/"
-"touch respect; rm respect; ls respect || echo le respect a disparu"
-"ls | > file_test echo test1 ; cat file_test ; rm -f file_test"
-"ls ;; ls"
-"ls ; | ls"
+    declare -a tests=(
 
-# /bin/kill -11 0
-# cat /dev/urandom | ./42sh
-# ./42sh < /dev/urandom
-# etc...
-)
+	"ls;ls"
+	"ls ; ls"
+	"|"
+	""
+	"&"
+	"&&"
+	"||"
+	";"
+	";;"
+	"cat Makefile"
+	"CEBINAIRENEXISTEPAS"
+	"pwd && cd .. && cd - && pwd"
+	"ls | grep include"
+	"ls|grep include"
+	"ls | grep include && echo ok || echo ko"
+	"ls | grep e | grep a > toto ; cat toto ; rm toto"
+	"ls > testfile | ls < EOF"
+	"ls | > testfile"
+	"cat toto"
+	"cat -e Makefile | grep 'CC'"
+	"ls /root || ls"
+	"cat << EOF | less"
+	"who"
+	"cat << EOF\ncoucou\nsalut\nEOF"
+	"echo echo echo ls | $SHELL | $SHELL | $SHELL "
+	"touch toto ; touch test ; cat < toto | wc -l < test ; rm toto ; rm test"
+	"mkdir lol && touch lol/test && ls > ls_out lol ; cat ls_out ; rm -Rf lol ; rm -f ls_out"
+	"echo /*/*/"
+	"touch respect; rm respect; ls respect || echo le respect a disparu"
+	"ls | > file_test echo test1 ; cat file_test ; rm -f file_test"
+	"ls ;; ls"
+	"ls ; | ls"
+	"cat /dev/urandom | ./42sh"
+	"./42sh < /dev/urandom"
+    )
+else
+    declare -a tests=(
+
+	"|"
+	""
+	";"
+	";;"
+	"ls;ls"
+	"ls ; ls"
+	"cd; pwd;"
+	"cd ..; pwd;"
+	"cd ././././././; pwd;"
+	"cd -; pwd;"
+	"cat Makefile"
+	"CEBINAIRENEXISTEPAS"
+	"ls | grep include"
+	"ls|grep include"
+	"ls | grep e | grep a > toto ; cat toto ; rm toto ; cat toto;"
+	"ls > testfile | ls < EOF"
+	"ls | > testfile"
+	"cat toto"
+	"cat -e Makefile | grep 'CC'"
+	"cat << EOF | less"
+	"who"
+	"cat << EOF\ncoucou\nsalut\nEOF"
+	"touch toto ; touch test ; cat < toto | wc -l < test ; rm toto ; rm test"
+	"ls ;; ls"
+	"ls ; | ls"
+	"cat /dev/urandom | ./mysh"
+	"./mysh < /dev/urandom"
+    )
+fi
 
 for i in "${tests[@]}"
 do
@@ -96,5 +127,5 @@ LS_END=`ls`
 
 #Know if files were created
 if [[ $LS_BEGIN != $LS_END ]] ; then
-  echo -e "${BRed}Some files were created${RCol}"
+  echo -e "${BRed}Certains fichiers n'ont pas été supprimé, la raison la plus probable étant que certains tests ont planté :)${RCol}"
 fi
